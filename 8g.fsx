dafc2d7e81944f2d8296c566497c91c5550e714d
SaxumYt <Saxumyt@gmail.com>
2016-11-07 12:09:00 +0100

    obliatoriske funktioner og typer

--- 8g.fsx
+++ 8g.fsx
@@ -1,4 +1,18 @@
-//It begins...
-//test
-//test2
-trolden under broen
+type codeColor =
+    Red | Green | Yellow | Purple | White | Black
+type code = codeColor list
+type answer = int * int
+type board = ( code * answer ) list
+type player = Human | Computer
+
+
+
+
+let makecode (player1: player) =
+    ()
+
+let guess (player2: player) (gameBoard: board) =
+    ()
+
+let validate (secretCode: code) (playerGuess: code) =
+    ()
